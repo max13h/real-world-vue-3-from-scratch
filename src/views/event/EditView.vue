64e9807c48afe0383807206c51b3cfcc5f224b44
maxime hmae <max.hmae@gmail.com>
2023-08-02 10:17:55 +0200

    progammatic navigation on register and edit btn

--- src/views/event/EditView.vue
+++ src/views/event/EditView.vue
@@ -2,12 +2,18 @@
   <div>
     <h3>Click to edit this event</h3>
     <br>
-    <button type="button" class="btn-primary" data-btn="Edit event">Edit event</button>
+    <button type="button" class="btn-primary" data-btn="Edit event" @click="register">Edit event</button>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from "vue-router";
 
+const router = useRouter()
+
+const register = () => {
+  router.push({ name: 'event-details' })
+}
 </script>
 
 <style scoped>
--- src/views/event/RegisterView.vue
+++ src/views/event/RegisterView.vue
@@ -2,12 +2,18 @@
   <div>
     <h3>Click to register to this event</h3>
     <br>
-    <button type="button" class="btn-primary" data-btn="Register Me">Register Me</button>
+    <button type="button" class="btn-primary" data-btn="Register Me" @click="register">Register Me</button>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from "vue-router";
 
+const router = useRouter()
+
+const register = () => {
+  router.push({ name: 'event-details' })
+}
 </script>
 
 <style scoped>
