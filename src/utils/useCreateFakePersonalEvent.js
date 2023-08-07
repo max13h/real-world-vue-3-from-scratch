315656d81e08a819707a8ad2f70ff0a6db5db740
maxime hmae <max.hmae@gmail.com>
2023-08-07 12:53:40 +0200

    formating date in quick create event

--- src/utils/useCreateFakePersonalEvent.js
+++ src/utils/useCreateFakePersonalEvent.js
@@ -1,17 +1,21 @@
 import { useEventsStore } from "../store/events"
 import { faker } from '@faker-js/faker'
+import dayjs from 'dayjs/dayjs.min'
 
 export function useCreateFakePersonalEvent() {
   const eventsStore = useEventsStore()
 
   eventsStore.personalEventsID++
 
+  const fakerDate = faker.date.soon()
+  const date = dayjs(fakerDate).format("YYYY-MM-DD HH:mm")
+
   const newEvent = {
     id: eventsStore.personalEventsID,
     title: faker.lorem.words(3),
     description: faker.lorem.words({ min: 4, max: 10 }),
     location: faker.location.city(),
-    date: faker.date.soon(),
+    date: date,
     organizer: faker.company.name(),
   }
 
